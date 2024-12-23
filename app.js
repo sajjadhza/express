e50774c58fd487faf33596c15d213f3d4d0f9302
sajjadhza <sa1ho3za77@gmail.com>
2024-12-23 10:16:56 +0330

    ADD : express lesson

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const http = require("http");
 const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
   console.log("this is first middleware");
-  next()
+  next();
 });
 
 app.use((req, res, next) => {
   console.log("this is second middleware");
+  res.send("<h1> Hello lovely express</h1>");
 });
 
-const server = http.createServer(app);
-server.listen(3000);
+app.listen(3000);
