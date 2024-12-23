5eacdfdc43a797557a3fb32d2b6aaa293ef9538a
sajjadhza <sa1ho3za77@gmail.com>
2024-12-23 10:55:32 +0330

    ADD : body-parser

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("this is first middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input name='title' type='text'/><button type='submit'>Add Product</button></form>"
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("this is second middleware");
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("this is home page");
   res.send("<h1> Hello lovely express</h1>");
 });
 
